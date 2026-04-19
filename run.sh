d5f85f035a882cd02c2fd5743ec365d6dd9d717e
yamaki-87 <gyt3218@gmail.com>
2026-04-19 20:09:05 +0900

    bumpallocator実装

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 -no-reboot -no-shutdown -serial stdio -drive format=raw,file=$HOME/study/myos/target/debug/build/myos-5f2ca893d0b9eedb/out/os-bios.img
+qemu-system-x86_64 -no-reboot -no-shutdown -serial stdio -drive format=raw,file=$HOME/study/myos/target/debug/build/myos-3b491c3e4241445c/out/os-bios.img
